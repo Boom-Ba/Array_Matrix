e392e1606b480664f8b11ac480904874bff1b284
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-15 11:10:44 -0500

    Create Shrink_Array.py

---
+++ Array/Triplets/Recursion/Shrink_Array.py
@@ -0,0 +1,32 @@
+# Given an integer array and an integer k, 
+#shrink it by removing adjacent triplets that 
+#satisfy the given constraints and return the left number of elements in the resultant array.
+class Solution:
+	def shrink(self, nums: List[int], k: int) -> int:
+		
+		##recursion on each ele in nums
+		
+		#two cases:
+		
+		#1.include the current element in triplet
+		#2.not 
+		
+		def solve(start,end, nums):
+			if start>end:
+				return 0
+			#1.exclude, so this element has to be removed 
+			res = solve(start+1,end,nums)+1 
+			
+			
+			#find tuplet (start, i,j )
+			for i in range(start+1,end):
+				for j in range(i+1,end+1):
+					
+					if nums[start]+k==nums[i] and nums[i]+k ==nums[j]:
+						if not solve(start+1,i-1,nums) and not solve(i+1,j-1,nums):
+						#found a tuplet
+							n= solve(j+1,end,nums)
+					
+							res= min(n,res)
+			return res
+		return solve(0,len(nums)-1, nums)
